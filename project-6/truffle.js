5cd81efc599bbf88ebccdfc123f95ee47dc7de07
heitho <thomasheil@Thomass-MacBook-Pro.local>
2020-06-05 19:23:14 +0200

    finished

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -1,9 +1,23 @@
+const HDWallet = require('@truffle/hdwallet-provider');
+const infuraKey = "0a9306187547488980976ecece1c21b9";
+//  
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 8545,
+      port: 7545,
       network_id: "*" // Match any network id
-    }
+    },
+    ropsten: {
+      provider: () => new HDWallet(mnemonic, `https://ropsten.infura.io/v3/${infuraKey}`),
+      network_id: 3,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
   }
 };
